d7046e65179455a888365e5b8d837dd48b7537a5
gramsa23 <89297908+gramsa23@users.noreply.github.com>
2022-03-05 21:38:47 +0300

    practice

---
+++ Practice.Core/Models/ProductFeature.cs
@@ -0,0 +1,13 @@
+namespace Practice.Core
+{
+    public class ProductFeature
+    {
+        public int Id { get; set; }
+        public string? Color { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+    }
+}
